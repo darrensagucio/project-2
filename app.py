6c99271b1b2204b8f3595bc53bfe94d153e49b01
Darren Sagucio <darrensagucio@Darrens-MacBook-Pro.local>
2021-06-12 11:34:16 -0700

    commit

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template, jsonify, url_for, json
+import numpy as np
+import os
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/mapdata")
+def mapdata(): 
+    
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", "united_states.json")
+    usdata = json.load(open(json_url))
+    
+    return usdata
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZGFycmVuc2FndWNpbyIsImEiOiJja3A0c3NmdTQyM2ozMnVtdzhjOWk5ajQ0In0.O1DnEf_Ttdd5XQ1N8w7AHw";
