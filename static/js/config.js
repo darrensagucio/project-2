1bfec1ede1fa554b4af4c64d0b07bc6963582622
admngz <78049309+admngz@users.noreply.github.com>
2021-06-15 11:00:48 -0700

    added image link

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiZGFycmVuc2FndWNpbyIsImEiOiJja3A0c3NmdTQyM2ozMnVtdzhjOWk5ajQ0In0.O1DnEf_Ttdd5XQ1N8w7AHw";
+const API_KEY = "pk.eyJ1IjoiYWRtbmd6IiwiYSI6ImNrcDR0NHhudzFzZ2Myd3RhZnJxa3FmeGsifQ.CEV6z_flY0FwHr3CwwFvKA";
