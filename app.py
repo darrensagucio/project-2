ef95d87e27084e54f5418a8a8fc5764690af01cb
Darren Sagucio <darrensagucio@Darrens-MacBook-Pro.local>
2021-06-15 17:15:13 -0700

    commit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify, url_for, json
 import numpy as np
 import os
+import pandas as pd
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -37,6 +38,87 @@ def mapdata():
     json_url = os.path.join(SITE_ROOT, "static/data", "united_states.json")
     usdata = json.load(open(json_url))
     
+    return usdata
+
+@app.route("/realdata")
+def realdata(): 
+
+    # Read CSV
+    df = pd.read_csv('static/data/MASTER.csv')
+
+    # Year
+    only2019 = df.loc[df['Year'] == 2019]
+
+    # Rename DF columns 
+    rename_only_2019 = only2019.rename({'Food Insecurity Rate (aggregate of counties)': 'Rates',
+                                   ' Estimated Number Food Insecure Individuals ': 'Individuals'}, axis='columns')
+
+    # Create New Data Frame
+    df_almost_clean = rename_only_2019[["State Name", "Year", "State Abbreviation ","Rates","Individuals"]]
+
+    # Strip %
+    series = df_almost_clean['Rates'].str.strip('%')
+    ratesframe = series.to_frame()
+    rename_rates_frame = ratesframe.rename({'Rates':'RatesNum'}, axis='columns')
+    rename_rates_frame['RatesNum'] = rename_rates_frame.RatesNum.astype(float)
+
+    # Strip Comma From Numbers
+    strip_comma = df_almost_clean['Individuals'].str.replace(',', '')
+    individuals_frame_1 = strip_comma.to_frame()
+    individuals_frame_1['Individuals'] = individuals_frame_1.Individuals.astype(int)
+
+    # Add New Columns To DF
+    df_almost_clean.insert(3,"RatesNum", rename_rates_frame, True)
+    df_almost_clean.insert(5,"IndNum", individuals_frame_1, True)
+
+    # Drop Columns 
+    df_almost_clean1 = df_almost_clean.drop(columns=["Rates"])
+
+    # Copy DF
+    df_2019_cleanup = df_almost_clean1.copy()
+
+
+    df_2019_cleanup['State Name'] = df_2019_cleanup['State Name'].replace({'South Dekota':'South Dakota', 'Minneapolis':'Minnesota'})
+    
+    df_2019_clean = df_2019_cleanup.copy()
+
+    # Clean Data
+    tojson = df_2019_clean.to_json(orient="table")
+    results = json.loads(tojson)
+
+    #########################################
+    # Pull Other Data 
+    SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT, "static/data", "united_states.json")
+    usdata = json.load(open(json_url))
+
+    # Delete Puerto Rico
+    for i in range(len(usdata["features"])): 
+        if usdata['features'][i]['properties']['NAME'] == 'Puerto Rico':
+            usdata['features'].pop(i)
+            break
+    # usdata['features'].pop(16)
+
+    # Rename Washington D.C
+    usdata['features'][25]['properties']['NAME'] = usdata['features'][25]['properties']['NAME'].replace('District of Columbia', 'Washington, D.C.')
+    ##########################################
+
+    Rates_Num_Array = []
+    Ind_Num_Array = []
+    Ind_String_Array = []
+
+    for i in range(len(results["data"])):
+        Rates_Num_Array.append(results['data'][i]['RatesNum'])
+        Ind_Num_Array.append(results['data'][i]['IndNum'])
+        Ind_String_Array.append(results['data'][i]['Individuals'])
+    
+    for i in range(len(usdata["features"])):
+        for j in range(len(usdata["features"])):
+            if usdata['features'][i]['properties']['NAME'] == results['data'][j]['State Name']:
+                usdata['features'][i]['properties']['RatesNum'] = Rates_Num_Array[j]
+                usdata['features'][i]['properties']['IndNum'] = Ind_Num_Array[j]
+                usdata['features'][i]['properties']['IndString'] = Ind_String_Array[j]
+
     return usdata
 
 if __name__ == "__main__":
--- static/js/map.js
+++ static/js/map.js
@@ -1,28 +1,140 @@
+// var myMap = L.map("map", {
+//     center: [37.8,-96],
+//     zoom: 4
+// });
+
+// L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//     maxZoom: 18,
+//     id: "light-v10",
+//     accessToken: API_KEY
+// }).addTo(myMap);
+
+// function getColor(d){
+//     return d > 50 ? '#282828' :
+//             d > 40 ? '#787878' : 
+//             d > 30 ? '#989898' :
+//             d > 15 ? '#B8B8B8' :
+//                       '#D3D3D3';
+                      
+// }
+
+// function style (info) {
+//     return {
+//         color: "white",
+//         fillColor: getColor(parseInt(info.properties.STATE)),
+//         fillOpacity: 1,
+//         weight: 1.5
+//     }
+// }
+// var info = L.control(); 
+
+// info.onAdd = function (map) {
+//     this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+//     this.update();
+//     return this._div;
+// };
+
+// info.update = function (label) {
+//     this._div.innerHTML = '<h4>Food Insecurity In The U.S</h4>'
+// }; 
+
+// info.addTo(myMap);
+
+// function highlightFeature(e) {
+//     var layer = e.target; 
+
+//     layer.setStyle({
+//         weight: 5,
+//         color: '#667',
+//         dashArray: '',
+//         fillOpaciity: 0.7
+//     });
+
+//     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+//         layer.bringToFront();
+//     }
+// }
+
+// var geojson; 
+
+// function resetHighlight(e) {
+//     geojson.resetStyle(e.target);
+// }
+
+// function onEachFeature(feature, layer) {
+//     layer.on({
+//         mouseover: highlightFeature,
+//         mouseout: resetHighlight
+//     })
+//     layer.on('click', function() {
+//         layer.bindPopup('<h2>' + feature.properties.NAME + '</h2><h2>' + feature.properties.STATE + '</h2>').openPopup();
+//     })
+// }
+
+// var legend = L.control({position: 'bottomright'});
+
+// legend.onAdd = function () {
+
+//     var div = L.DomUtil.create('div', 'info legend'),
+//         grades = [0, 15, 30, 40, 50];
+//         labels = [];
+
+//     for (var i = 0; i < grades.length; i++) {
+//         div.innerHTML +=
+//         '<i style="background:' + getColor(grades[i]+1) + '"></i> ' +
+//         grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+//     }
+
+//     return div;
+// };
+
+// legend.addTo(myMap);
+
+
+// d3.json("http://127.0.0.1:5000/mapdata").then(function(mapData) {
+//   var data = mapData;
+//   console.log(data);
+//   geojson = L.geoJson(data, {
+//     // Passing in our style object
+//     style: style,
+//     dashArray: 3,
+//     onEachFeature: onEachFeature
+//   }).addTo(myMap);
+// }).catch(function (error) {
+// 	console.log(error);
+// });
+/// 
 var myMap = L.map("map", {
     center: [37.8,-96],
     zoom: 4
-});
+  });
 
 L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
-}).addTo(myMap);
+  }).addTo(myMap);
+
+console.log("is this working??");
+console.log(API_KEY);
 
 function getColor(d){
-    return d > 50 ? '#282828' :
-            d > 40 ? '#787878' : 
-            d > 30 ? '#989898' :
-            d > 15 ? '#B8B8B8' :
-                      '#D3D3D3';
+    return d > 20.0 ? '#FF4F33' :
+            d > 14.0 ? '#BA55D3' :
+            d > 12.0 ? '#F9FF33' : 
+            d > 10.0 ? '#FFC0CB' : 
+            d > 8.0 ? '#4FFF33' :
+            d > 4.0 ? '#3380FF' :
+                      '#000000';
                       
 }
 
 function style (info) {
     return {
         color: "white",
-        fillColor: getColor(parseInt(info.properties.STATE)),
+        fillColor: getColor(info.properties.RatesNum),
         fillOpacity: 1,
         weight: 1.5
     }
@@ -41,6 +153,26 @@ info.update = function (label) {
 
 info.addTo(myMap);
 
+// var mapStyle = {
+//     color: "white",
+//     fillColor: "black",
+//     fillOpacity: 0.5,
+//     weight: 1.5
+// };
+
+randomNumArray = []; 
+
+for (var i = 0; i < 52; i++) {
+    var randomNum = Math.floor(Math.random() * 1000);
+    randomNumArray.push(randomNum)
+}
+
+
+// console.log(features.properties. )
+console.log(randomNumArray.length)
+console.log(randomNumArray)
+// var info = L.control();
+
 function highlightFeature(e) {
     var layer = e.target; 
 
@@ -54,12 +186,16 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+
+    // info.update(layer.feature.properties);
 }
 
 var geojson; 
+var geojson1;
 
 function resetHighlight(e) {
     geojson.resetStyle(e.target);
+    // info.update();
 }
 
 function onEachFeature(feature, layer) {
@@ -68,21 +204,28 @@ function onEachFeature(feature, layer) {
         mouseout: resetHighlight
     })
     layer.on('click', function() {
-        layer.bindPopup('<h2>' + feature.properties.NAME + '</h2><h2>' + feature.properties.STATE + '</h2>').openPopup();
+        // layer.bindPopup('<h2>' + feature.properties.RatesNum + '%</h2><h2>' + feature.properties.NAME + '</h2>').openPopup();
+        layer.bindPopup('<h2>' + feature.properties.NAME + '</h2><h2> Rate: ' + feature.properties.RatesNum + '%</h2><h2> Insecure Individuals: ' + feature.properties.IndString + '</h2>').openPopup();
     })
 }
+
+// function onEachFeatureToolTip(feature, layer) {
+//     layer.on('click', function() {
+//         layer.bindPopup('<h2>' + feature.precipitation + '</h2><h2>' + feature.properties.STATE + '</h2>').openPopup();
+//     })
+// }
 
 var legend = L.control({position: 'bottomright'});
 
 legend.onAdd = function () {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 15, 30, 40, 50];
+        grades = [0, 4.0, 8.0, 10.0, 12.0, 14.0, 20.0];
         labels = [];
 
     for (var i = 0; i < grades.length; i++) {
         div.innerHTML +=
-        '<i style="background:' + getColor(grades[i]+1) + '"></i> ' +
+        '<i style="background:' + getColor(grades[i]+ .01) + '"></i> ' +
         grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
     }
 
@@ -91,16 +234,33 @@ legend.onAdd = function () {
 
 legend.addTo(myMap);
 
+function init () {
+    d3.json("http://127.0.0.1:5000/realdata").then(function(mapData) {
+        var data = mapData;
+        console.log("hi");
+        console.log("INIT");
+        console.log(data);
+        console.log(parseInt(data.features[0].properties.STATE))
+        console.log("NAMES");
+        //   var nameState = []; 
+        //   for (var i = 0; i < 52; i++) {   
+        //     nameState.push(data.features[i].properties.NAME)
+        //   }
+        //   console.log(nameState);
+        //   console.log(typeof data.weatherdata[2].precipitation)
+        //   console.log(data.weatherdata[2].precipitation)
+        //   console.log(data.weatherdata.precipitation)
+        //   var m = 1;
+        //   console.log(typeof m)
+        geojson = L.geoJson(data, {
+            // Passing in our style object
+            style: style,
+            dashArray: 3,
+            onEachFeature: onEachFeature
+        }).addTo(myMap);
+        }).catch(function (error) {
+            console.log(error);
+        });
+}
 
-d3.json("http://127.0.0.1:5000/mapdata").then(function(mapData) {
-  var data = mapData;
-  console.log(data);
-  geojson = L.geoJson(data, {
-    // Passing in our style object
-    style: style,
-    dashArray: 3,
-    onEachFeature: onEachFeature
-  }).addTo(myMap);
-}).catch(function (error) {
-	console.log(error);
-});
+init();
